bce9a25531f119b9550aed2ef81c199cfaf16fe9
chingis oinar <chingisoinar@gmail.com>
2024-09-10 11:25:45 +0900

    feat: default reduction strategies for GroupByReduce

--- feature_fabrica/transform/aggregate.py
+++ feature_fabrica/transform/aggregate.py
@@ -1,39 +1,78 @@
 import numpy as np
 from beartype import beartype
+from scipy import stats
 
 from feature_fabrica.transform.base import Transformation
-from feature_fabrica.transform.utils import StrOrNumArray
+from feature_fabrica.transform.utils import NumericArray, StrOrNumArray
 
+
+@beartype
+def mean_reduction(data: NumericArray, axis: int) -> NumericArray:
+    return np.mean(data, axis=axis)
+
+@beartype
+def min_reduction(data: NumericArray, axis: int) -> NumericArray:
+    return np.min(data, axis=axis)
+
+@beartype
+def max_reduction(data: NumericArray, axis: int) -> NumericArray:
+    return np.max(data, axis=axis)
+
+@beartype
+def median_reduction(data: NumericArray, axis: int) -> NumericArray:
+    return np.median(data, axis=axis)
+
+@beartype
+def mode_reduction(data: NumericArray, axis: int) -> NumericArray:
+    # use LabelEncode on your feature first: https://github.com/scipy/scipy/issues/15551
+    mode_result = stats.mode(data, axis=axis)
+    # Extract the mode from the result
+    return mode_result.mode
+
+DEAFULT_REDUCTIONS = dict(
+        mean = mean_reduction,
+        min = min_reduction,
+        max = max_reduction,
+        median = median_reduction,
+        mode = mode_reduction
+    )
 
 class GroupByReduce(Transformation):
     @beartype
-    def __init__(self, key_feature: str, reduce_func: Transformation, axis: int = 0):
+    def __init__(self, key_feature: str, reduce_func: Transformation | str = "mean", axis: int = -1):
 
+        if isinstance(reduce_func, str):
+            assert reduce_func in ["mean", "mode", "min", "max", "median"]
+            reduce_func = DEAFULT_REDUCTIONS[reduce_func]
         self.key_feature = key_feature
         self.reduce_func = reduce_func
         self.axis = axis
+
     @beartype
     def execute(self, data: StrOrNumArray) -> StrOrNumArray:
         """Groups data array by key_feature values and applies a reduction transformation.
 
         Assigns reduced values to the original positions to preserve the original shape.
         """
-        assert self.reduce_func.expects_data
+        if isinstance(self.reduce_func, Transformation):
+            assert self.reduce_func.expects_data
         #assert isinstance(self.key_feature, FeatureValue), "key_feature must be an existing feature!"
         assert self.key_feature.shape == data.shape # type: ignore[attr-defined]
-        key_feature = self.key_feature
+        key_feature_value = self.key_feature.value # type: ignore[attr-defined]
 
-        sorted_idxs = key_feature.argsort() # type: ignore[attr-defined]
+        sorted_idxs = key_feature_value.argsort() # type: ignore[attr-defined]
         data_sorted = data[sorted_idxs]
-        key_feature_sorted = key_feature[sorted_idxs]
+        key_feature_sorted = key_feature_value[sorted_idxs]
 
         data_aggregated =  np.split(data_sorted, np.unique(key_feature_sorted, return_index=True)[1][1:])
 
         data_aggregated = np.array(data_aggregated, dtype=data.dtype)
-        data_reduced = self.reduce_func.execute(data_aggregated)
-
+        if isinstance(self.reduce_func, Transformation):
+            data_reduced = self.reduce_func.execute(data_aggregated)
+        else:
+            data_reduced = self.reduce_func(data_aggregated, self.axis) # type: ignore[operator]
         # Use np.unique to find the unique key values and the counts for each
-        _, inverse_indices = np.unique(key_feature, return_inverse=True)
+        _, inverse_indices = np.unique(key_feature_value, return_inverse=True)
 
         # Use np.repeat to assign the reduced values to the original positions
         result = data_reduced[inverse_indices]
--- tests/test_aggregate_transform.py
+++ tests/test_aggregate_transform.py
@@ -4,6 +4,7 @@ import numpy as np
 from easydict import EasyDict as edict
 from numpy.testing import assert_array_almost_equal, assert_array_equal
 
+from feature_fabrica.models import FeatureValue
 from feature_fabrica.transform import (ConcatenateReduce, GroupByReduce,
                                        SumReduce)
 
@@ -11,22 +12,25 @@ from feature_fabrica.transform import (ConcatenateReduce, GroupByReduce,
 class TestTransformations(unittest.TestCase):
     def test_sum_reduce(self):
         transform = GroupByReduce('feature', SumReduce(expects_data=True, axis=-1))
-        example = edict({'feature': {'feature_value': np.array([1,1,2,2,3,3])}})
+        feature_value = FeatureValue(value=np.array([1, 1, 2, 2, 3, 3]), data_type='int32')
+        example = edict({'feature': {'feature_value': feature_value}})
         transform.compile(example)
-        expected = np.array([ 5,  5,  9,  9, 13, 13])
-        data = np.array([2,3,4,5,6,7])
+        expected = np.array([5, 5, 9, 9, 13, 13])
+        data = np.array([2, 3, 4, 5, 6, 7])
         actual = transform.execute(data)
         assert_array_almost_equal(actual, expected)
 
 
         transform = GroupByReduce('feature', SumReduce(expects_data=True, axis=-1))
-        example = edict({'feature': {'feature_value': np.array(['1','1','2','2','3','3'])}})
+        feature_value = FeatureValue(value=np.array(['1', '1', '2', '2', '3', '3']), data_type='str_')
+        example = edict({'feature': {'feature_value': feature_value}})
         transform.compile(example)
         actual = transform.execute(data)
         assert_array_almost_equal(actual, expected)
 
         transform = GroupByReduce('feature', ConcatenateReduce(expects_data=True, axis=-1))
-        example = edict({'feature': {'feature_value': np.array(['1','1','2','2','3','3'])}})
+        feature_value = FeatureValue(value=np.array(['1','1','2','2','3','3']), data_type='str_')
+        example = edict({'feature': {'feature_value': feature_value}})
         transform.compile(example)
         data = np.array(['2','3','4','5','6','7'])
         actual = transform.execute(data)
@@ -34,12 +38,42 @@ class TestTransformations(unittest.TestCase):
         assert_array_equal(actual, expected)
 
         transform = GroupByReduce('feature', ConcatenateReduce(expects_data=True, axis=-1))
-        example = edict({'feature': {'feature_value': np.array([1,1,2,2,3,3])}})
+        feature_value = FeatureValue(value=np.array([1, 1, 2, 2, 3, 3]), data_type='int32')
+        example = edict({'feature': {'feature_value': feature_value}})
         transform.compile(example)
         data = np.array(['2','3','4','5','6','7'])
         actual = transform.execute(data)
         expected = np.array(['23', '23', '45', '45', '67', '67'])
         assert_array_equal(actual, expected)
+
+        for mode in ['mean', 'median']:
+            transform = GroupByReduce('feature', mode, axis=-1)
+            feature_value = FeatureValue(value=np.array([1, 1, 2, 2, 3, 3]), data_type='int32')
+            example = edict({'feature': {'feature_value': feature_value}})
+            transform.compile(example)
+            data = np.array([5, 1, 9, 1, 13, 1])
+            expected = np.array([3, 3, 5, 5, 7, 7])
+            actual = transform.execute(data)
+            assert_array_almost_equal(actual, expected)
+
+        transform = GroupByReduce('feature', 'max', axis=-1)
+        feature_value = FeatureValue(value=np.array([1, 1, 2, 2, 3, 3]), data_type='int32')
+        example = edict({'feature': {'feature_value': feature_value}})
+        transform.compile(example)
+        data = np.array([5, 1, 9, 1, 13, 1])
+        expected = np.array([5, 5, 9, 9, 13, 13])
+        actual = transform.execute(data)
+        assert_array_almost_equal(actual, expected)
+
+        transform = GroupByReduce('feature', 'mode', axis=-1)
+        feature_value = FeatureValue(value=np.array([1, 1, 1, 3, 3, 3]), data_type='int32')
+        example = edict({'feature': {'feature_value': feature_value}})
+        transform.compile(example)
+        data = np.array([5, 1, 5, 1, 13, 1])
+        expected = np.array([5, 5, 5, 1, 1, 1])
+        actual = transform.execute(data)
+        assert_array_almost_equal(actual, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
