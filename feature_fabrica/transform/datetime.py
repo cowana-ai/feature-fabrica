c7e059f8540871d1e9d75ad28277619ce1238f0b
chingis oinar <chingisoinar@gmail.com>
2024-09-14 14:45:50 +0900

    doc: docstring

--- feature_fabrica/transform/datetime.py
+++ feature_fabrica/transform/datetime.py
@@ -10,6 +10,34 @@ from feature_fabrica.transform.utils import (DateTimeArray, StrArray,
 class DateTimeDifference(Transformation):
     @beartype
     def __init__(self, initial_datetime: str | None = None, end_datetime: str | None = None, compute_unit: str | None = None):
+        """This transformation computes the difference between a provided datetime array and an initial or end datetime.
+        The difference is computed in the specified `compute_unit` (e.g., seconds, minutes, hours). This can be used to
+        compute such features as recency or time difference.
+
+        Parameters
+        ----------
+        initial_datetime : str | None, optional (can be feature)
+            The initial datetime string, formatted as a NumPy-compatible datetime.
+            If set, the difference will be computed as the data minus the initial
+            datetime. Either `initial_datetime` or `end_datetime` must be set, but
+            not both.
+        end_datetime : str | None, optional (can be feature)
+            The end datetime string, formatted as a NumPy-compatible datetime.
+            If set, the difference will be computed as the end datetime minus the
+            data. Either `end_datetime` or `initial_datetime` must be set, but
+            not both.
+        compute_unit : str | None, optional
+            The unit in which to compute the datetime difference, such as 's' for
+            seconds or 'D' for days. Valid values are ['as', 'fs', 'ps', 'ns', 'us',
+            'ms', 's', 'm', 'h', 'D', 'W', 'M', 'Y']. The default is None, which
+            uses the default datetime unit.
+
+        Raises
+        ------
+        ValueError
+            If both `initial_datetime` and `end_datetime` are set or if neither is
+            set. Also raised if `compute_unit` is not a valid code.
+        """
         super().__init__()
         if (initial_datetime and end_datetime) or (initial_datetime is None and end_datetime is None):
             raise ValueError("Only one of 'initial_datetime' or 'end_datetime' should be set!")
