7f6805f029f8369acbb1cdca39e4eca20e9ce8c6
chingis oinar <chingisoinar@gmail.com>
2024-09-17 18:25:33 +0900

    merge main

--- tests/test_core.py
+++ tests/test_core.py
@@ -74,8 +74,10 @@ class TestFeatureSet(unittest.TestCase):
         np.testing.assert_array_equal(results["feature_e_upper"], expected_feature_e_upper)
         np.testing.assert_array_equal(results["feature_e"], expected_feature_e)
         np.testing.assert_array_equal(results["feature_e"], results["feature_e_one_hot"])
+
         np.testing.assert_array_equal(results["feature_e_upper_lower_original"], expected_feature_e_upper_lower_original)
         np.testing.assert_array_equal(results["feature_abd"], expected_feeture_abd)
+
 
         # Assert FeatureValue
         np.testing.assert_array_equal(
